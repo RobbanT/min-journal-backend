79d9d05887345f5294eaa2e0782839699aaffa53
Roberto Tencic <roberto.tencic@outlook.com>
2024-10-04 19:44:55 +0200

    Färdig

--- src/main/java/com/minjournalbackend/minjournalbackend/MinjournalbackendApplication.java
+++ src/main/java/com/minjournalbackend/minjournalbackend/MinjournalbackendApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MinjournalbackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MinjournalbackendApplication.class, args);
 	}
-
 }
